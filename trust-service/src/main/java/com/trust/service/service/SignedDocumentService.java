3282fe375410a14a597c74eec922118e953936c5
Balša Šarenac <balsa.smi15@gmail.com>
2022-09-20 22:35:41 +0200

    feat: document signing logic

--- trust-service/src/main/java/com/trust/service/service/SignedDocumentService.java
+++ trust-service/src/main/java/com/trust/service/service/SignedDocumentService.java
@@ -1,5 +1,7 @@
 package com.trust.service.service;
 
+import com.trust.service.model.Signature;
+import com.trust.service.repository.SignatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +11,43 @@ import com.trust.service.model.SignedDocument;
 import com.trust.service.model.SubmittedDocument;
 import com.trust.service.repository.SignedDocumentRepository;
 
+import java.util.Base64;
+
 @Service
 public class SignedDocumentService {
 	private final SignedDocumentRepository signedDocumentRepository;
 	private final SubmittedDocumentService submittedDocumentService;
 	private final OrgInVirtOrgService orgInVirtOrgService;
+    private final SignatureRepository signatureRepository;
 
-	@Autowired
+    @Autowired
 	public SignedDocumentService(SignedDocumentRepository signedDocumentRepository,
-			SubmittedDocumentService submittedDocumentService, OrgInVirtOrgService orgInVirtOrgService) {
+                                 SubmittedDocumentService submittedDocumentService, OrgInVirtOrgService orgInVirtOrgService, SignatureRepository signatureRepository) {
 		this.signedDocumentRepository = signedDocumentRepository;
 		this.submittedDocumentService = submittedDocumentService;
 		this.orgInVirtOrgService = orgInVirtOrgService;
-	}
+        this.signatureRepository = signatureRepository;
+    }
 
 	public void save(String apiKey, SignedDocumentDto signedDocumentDto) {
 		OrgInVirtOrg orgInVirtOrg = orgInVirtOrgService.findByApiKey(apiKey);
 		SubmittedDocument submittedDocument = submittedDocumentService
 				.findByOrgInVirtOrgAndName(orgInVirtOrg, signedDocumentDto.getName());
-		signedDocumentRepository.save(new SignedDocument(orgInVirtOrg, submittedDocument));
+
+		SignedDocument signedDocument = signedDocumentRepository.save(new SignedDocument(orgInVirtOrg, submittedDocument));
+
+        String signatureXML = signXML(submittedDocument);
+        Signature signature = new Signature(orgInVirtOrg, signedDocument, signatureXML);
+        signatureRepository.save(signature);
+	}
+
+	private String signXML(SubmittedDocument submittedDocument) {
+		byte[] decodedBytes = Base64.getDecoder().decode(submittedDocument.getEncodedContent());
+		String decodedXML = new String(decodedBytes);
+
+		XMLSigningService xmlSigningService = new XMLSigningService();
+		String signatureXML = xmlSigningService.signDocument(decodedXML);
+
+		return Base64.getEncoder().encodeToString(signatureXML.getBytes());
 	}
 }
